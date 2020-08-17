1fcf6fea973e7c3b05f5c9d889f22344f57f019a
rsalazaro <r.alejandro.salazaro@gmail.com>
2020-08-17 19:39:34 -0400

    Updates

---
+++ precache-manifest.0904d196f6d79899e015e7c2e365f7d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274fc8298685b7e2d1bd22d46ebc4d89",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "d2129a1e862362c6fbfe",
+    "url": "/portafolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30aa2f0ea61258e3c697",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2129a1e862362c6fbfe",
+    "url": "/portafolio/static/js/main.fe7dd3dd.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
